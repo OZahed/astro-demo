7148d48e28e1c6b9304454280accc008ebad748f
ferizoozoo <farhadzohoorz@gmail.com>
2024-06-28 17:36:53 +0330

    refactor: change params id on todos api [id]

--- src/pages/todos/api/todos/[id].ts
+++ src/pages/todos/api/todos/[id].ts
@@ -4,42 +4,26 @@ import { db, eq, Todo } from "astro:db";
 
 // GET tasks
 export const GET: APIRoute = async ({ params, request }) => {
-    let id = 0;
-    if (params?.id) {
-        id = parseInt(params.id);
-    }
-    const res = await db.select().from(Todo).where(eq(Todo.id, id));
+    const res = await db.select().from(Todo).where(eq(Todo.id, Number(params.id!)));
     return new Response(JSON.stringify({ res }))
 }
 
 // POST tasks
 export const POST: APIRoute = async ({ params, request }) => {
-    let id = 0;
-    if (params?.id) {
-        id = parseInt(params.id);
-    }
     const body = await request?.json();
-    await db.update(Todo).set({ ...body }).where(eq(Todo.id, id));
+    await db.update(Todo).set({ ...body }).where(eq(Todo.id, Number(params.id!)));
     return new Response(JSON.stringify({ message: 'Todo added', id: params.id }))
 }
 
 // DELETE tasks
 export const DELETE: APIRoute = async ({ params }) => {
-    let id = 0;
-    if (params?.id) {
-        id = parseInt(params.id);
-    }
-    await db.delete(Todo).where(eq(Todo.id, id ));
+    await db.delete(Todo).where(eq(Todo.id, Number(params.id!) ));
     return new Response(JSON.stringify({ message: 'Todo deleted', id: params.id }))
 }
 
 // change name
 export const PUT: APIRoute = async ({ params, request }) => {
-    let id = 0;
-    if (params?.id) {
-        id = parseInt(params.id);
-    }
     const body = await request?.json();
-    await db.update(Todo).set({ ...body }).where(eq(Todo.id, id ));
+    await db.update(Todo).set({ ...body }).where(eq(Todo.id, Number(params.id!)));
     return new Response(JSON.stringify({ message: 'Todo updated', id: params.id }))
 }
